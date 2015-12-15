641d70854f99b37edf1c1f24e72e422468458224
Rodrigo Otavio van den Berg Maia <rodrigo@devlandia.net>
2015-12-15 20:09:39 +0000

    Rubocoped

--- lib/disqus/model.rb
+++ lib/disqus/model.rb
@@ -1,30 +1,30 @@
 module Disqus
   class Model
     extend Disqus::Concerns::Buildable
-  
+
     def self.api_key
       fail 'DISQUS_API_KEY not configured. Please add DISQUS_API_KEY to your .env' if ENV['DISQUS_API_KEY'].nil?
-  
+
       ENV['DISQUS_API_KEY']
     end
-  
+
     def api_key
       Disqus::Model.api_key
     end
-  
+
     def self.make_request(url)
       http      = Curl.get url
       response  = JSON.parse(http.body_str)
-  
+
       fail "ERROR CODE #{response['code']} :: #{response['response']}" unless http.status == '200 OK'
-  
+
       response['response']
     end
-  
+
     def self.read_config_file
       file_path = "#{APPLICATION_PATH}/config/settings.yml"
       fail 'File config/settings.yml does not exist' unless File.exist? file_path
-  
+
       YAML.load_file file_path
     end
   end
