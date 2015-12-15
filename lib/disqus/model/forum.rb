3aca9f0449225e3f2d54b2da64f66bd1a1a9a61a
Rodrigo Otavio van den Berg Maia <rodrigo@devlandia.net>
2015-12-15 19:52:53 +0000

    Requires

--- lib/disqus/model/forum.rb
+++ lib/disqus/model/forum.rb
@@ -2,34 +2,35 @@ module Disqus
   class Forum < Disqus::Model
     attr_accessor :id, :category, :description, :founder, :settings, :url, :guidelines, :favicon, :language, :avatar,
                   :pk, :signedUrl, :raw_guidelines, :id, :channel, :name
-  
+
     def self.find(id)
       url       = "https://disqus.com/api/3.0/forums/details.json?api_key=#{api_key}&forum=#{id}"
       response  = make_request url
-  
+
       assemble_with_attributes response
     end
-  
+
     def founder
       return @founder if @founder.is_a? Disqus::User
-  
+
       @founder = Disqus::User.find @founder
     end
-  
+
     def posts
       return @posts if @posts.is_a? Array
-  
+
       @posts  = []
       items = posts_from_remote
       items.each { |post| @posts << Disqus::Post.assemble_with_attributes(post) }
-  
+
       @posts
     end
-  
+
     private
-  
+
     def posts_from_remote
       url = "https://disqus.com/api/3.0/forums/listPosts.json?api_key=#{api_key}&forum=#{id}"
       Disqus::Model.make_request url
     end
   end
+end
--- lib/disqus/model/user.rb
+++ lib/disqus/model/user.rb
@@ -3,29 +3,29 @@ module Disqus
     attr_accessor :id, :disable3rdPartyTrackers, :isPowerContributor, :connections, :isPrimary, :id, :numFollowers, :rep, :numFollowing,
                   :numPosts, :location, :isPrivate, :joinedAt, :username, :numLikesReceived, :about, :name, :url, :numForumsFollowing,
                   :profileUrl, :reputation, :avatar, :signedUrl, :isAnonymous, :active_forums
-  
+
     def self.find(id)
       url       = "https://disqus.com/api/3.0/users/details.json?api_key=#{api_key}&user=#{id}"
       response  = make_request url
-  
+
       Disqus::User.assemble_with_attributes response
     end
-  
+
     def active_forums
       return @active_forums unless @active_forums.nil?
-  
+
       @active_forums = []
       items = forums_from_remote
       items.each { |forum| @active_forums << Disqus::Forum.assemble_with_attributes(forum) }
-  
+
       @active_forums
     end
-  
+
     private
-  
+
     def forums_from_remote
       url = "https://disqus.com/api/3.0/users/listActiveForums.json?api_key=#{api_key}&user=#{id}"
-  
+
       Disqus::User.make_request url
     end
   end
