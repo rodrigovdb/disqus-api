42a7123e6f4ff94191fac1fa8c31e4ac1c6c014f
Rodrigo Otavio van den Berg Maia <rodrigo@devlandia.net>
2015-12-15 19:32:00 +0000

    Requires

--- lib/disqus.rb
+++ lib/disqus.rb
@@ -8,12 +8,12 @@ require "dotenv"
 require "disqus/version"
 
 require_relative "disqus/model/concerns/buildable"
-require_relative "model"
-#require_relative "model/thread"
-#require_relative "model/post"
-#require_relative "model/media"
-#require_relative "model/forum"
-#require_relative "model/user"
+require_relative "disqus/model"
+#require_relative "disqus/model/thread"
+#require_relative "disqus/model/post"
+#require_relative "disqus/model/media"
+#require_relative "disqus/model/forum"
+#require_relative "disqus/model/user"
 
 module Disqus
   def self.foo
